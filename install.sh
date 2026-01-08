284aaa321a5657b01bc66901974aa3854dede6a3
xiaoz <xiaoz93@outlook.com>
2026-01-08 13:55:08 +0800

    fix a bug

--- install.sh
+++ install.sh
@@ -17,8 +17,8 @@ install_python_deps(){
     python3 -m venv myenv
     source myenv/bin/activate
     # 安装 Pillow-SIMD 替代 Pillow
-    CC="cc -mavx2" pip3 install -U --force-reinstall pillow-simd
-    pip3 install -r requirements.txt
+    CC="cc -mavx2" pip install -U --force-reinstall pillow-simd
+    pip install -r requirements.txt
 }
 
 
